c59aa00bf83cf5641d2e6d703d8426c2d17cb3eb
SESILLIS <srakela@sesillis.com>
2016-03-19 16:11:17 +0530

    Initial Copy

---
+++ DemoWebAppl/DemoWebAppl/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoWebAppl.Startup))]
+namespace DemoWebAppl
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
